c691007355f46aa55082973f3c85a4b0d1a342e9
diablo1303 <diabloposeidon@hotmail.com>
2023-06-13 15:28:02 +0800

    代码校验修复

--- src/main/java/org/geelato/utils/SqlParams.java
+++ src/main/java/org/geelato/utils/SqlParams.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 /**
  * @author geemeta
- *
  */
 public class SqlParams extends SimpleBindings {
 
@@ -17,6 +16,7 @@ public class SqlParams extends SimpleBindings {
 
     /**
      * 构建参数，
+     *
      * @param params 参数params的数量需为偶数，格式如：参数1名,参数1值,参数2名,参数2值,参数3名,参数3值...
      * @return
      */
@@ -28,9 +28,9 @@ public class SqlParams extends SimpleBindings {
         int i = 0;
         String key = "";
         for (String param : params) {
-            if (i % 2 == 0)
+            if (i % 2 == 0) {
                 key = param;
-            else {
+            } else {
                 map.put(key, param);
             }
             i++;
